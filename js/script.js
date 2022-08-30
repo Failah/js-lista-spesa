491779aab675ed31593b5bf541e413ab0585cc01
ValerioScharizza <valerio.schiarizza@live.it>
2022-08-30 13:58:05 +0200

    Script Done

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
 console.log('JS OK!');
 
+// Consegna:
+// Data una lista della spesa, stampare sulla pagina (anche brutalmente,
+// basta che si vedano) gli elementi della lista individualmente con un
+// ciclo while.
+// Consigli:
+// - Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio
+//  del ciclo while
+// - Ricordiamoci di incrementare il contatore, o comunque di inserire una
+// condizione di uscita, all'interno ciclo while
+
+
+const list = ['Pomodori', 'Pane', 'Bevande', 'Carne', 'Zucchero'];
+
+let counter = 0;
+
+while (counter < list.length) {
+
+    console.log(list[counter]);
+    document.getElementById('shopping-list').append(list[counter]);
+
+    counter++;
+}
